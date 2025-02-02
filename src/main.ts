5d4f4829205cab7ba22d793e7f19911d1ae6066f
Joao Tavares <joao.tavares42@outlook.com>
2025-02-02 17:20:45 -0300

    change cors policy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://3.85.62.127',
+    origin: 'https://front.jvwallet.site/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
